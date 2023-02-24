d7a65cdde3ac51434e5276c3cc5c2e28c709217a
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-02-24 18:23:47 +0100

    Update von1.2aobfuscated.lua

--- von1.2aobfuscated.lua
+++ von1.2aobfuscated.lua
@@ -112,6 +112,7 @@ local gameId = game.PlaceId
 local ver = database.scripts.von.version
 local supported_games = database.scripts.von.supported_games
 local gameSupported = false
+local featuresLocalization
 
 for i, v in pairs(supported_games) do
 	if supported_games[gameId] then
@@ -119,8 +120,142 @@ for i, v in pairs(supported_games) do
 	end
 end
 
+function createFeature(type, name, parent)
+	if type == "bool" then
+		local Template = Instance.new("Frame")
+		local UICorner = Instance.new("UICorner")
+		local boolType = Instance.new("Frame")
+		local UICorner_2 = Instance.new("UICorner")
+		local textType = Instance.new("TextBox")
+		local UICorner_3 = Instance.new("UICorner")
+		local title = Instance.new("TextLabel")
+		local UIGradient = Instance.new("UIGradient")
+		
+		Template.Name = "Template"
+		Template.Parent = parent
+		Template.BackgroundColor3 = Color3.fromRGB(50, 50, 54)
+		Template.Size = UDim2.new(0, 300, 0, 25)
+
+		UICorner.CornerRadius = UDim.new(0, 10)
+		UICorner.Parent = Template
+
+		boolType.Name = "boolType"
+		boolType.Parent = Template
+		boolType.AnchorPoint = Vector2.new(0.5, 0.5)
+		boolType.BackgroundColor3 = Color3.fromRGB(37, 37, 40)
+		boolType.Position = UDim2.new(0.959999979, 0, 0.5, 0)
+		boolType.Size = UDim2.new(0, 20, 0, 20)
+		boolType.Visible = true
+
+		UICorner_2.CornerRadius = UDim.new(0, 5)
+		UICorner_2.Parent = boolType
+
+		textType.Name = "textType"
+		textType.Parent = Template
+		textType.AnchorPoint = Vector2.new(0.5, 0.5)
+		textType.BackgroundColor3 = Color3.fromRGB(37, 37, 40)
+		textType.Position = UDim2.new(0.899999976, 0, 0.5, 0)
+		textType.Size = UDim2.new(0, 50, 0, 20)
+		textType.Visible = false
+		textType.Font = Enum.Font.GothamMedium
+		textType.PlaceholderText = ".."
+		textType.Text = ""
+		textType.TextColor3 = Color3.fromRGB(255, 255, 255)
+		textType.TextScaled = true
+		textType.TextSize = 14.000
+		textType.TextWrapped = true
+
+		UICorner_3.CornerRadius = UDim.new(0, 5)
+		UICorner_3.Parent = textType
+
+		title.Name = "title"
+		title.Parent = Template
+		title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		title.BackgroundTransparency = 1.000
+		title.Size = UDim2.new(0, 200, 1, 0)
+		title.Text = name
+		title.Font = Enum.Font.GothamBlack
+		title.TextColor3 = Color3.fromRGB(255, 255, 255)
+		title.TextScaled = true
+		title.TextSize = 14.000
+		title.TextWrapped = true
+		title.TextXAlignment = Enum.TextXAlignment.Left
+
+		UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(0.47, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(146, 164, 182))}
+		UIGradient.Rotation = 90
+		UIGradient.Parent = title
+	elseif type == "text" then
+		local Template = Instance.new("Frame")
+		local UICorner = Instance.new("UICorner")
+		local boolType = Instance.new("Frame")
+		local UICorner_2 = Instance.new("UICorner")
+		local textType = Instance.new("TextBox")
+		local UICorner_3 = Instance.new("UICorner")
+		local title = Instance.new("TextLabel")
+		local UIGradient = Instance.new("UIGradient")
+
+		Template.Name = "Template"
+		Template.Parent = parent
+		Template.BackgroundColor3 = Color3.fromRGB(50, 50, 54)
+		Template.Size = UDim2.new(0, 300, 0, 25)
+
+		UICorner.CornerRadius = UDim.new(0, 10)
+		UICorner.Parent = Template
+
+		boolType.Name = "boolType"
+		boolType.Parent = Template
+		boolType.AnchorPoint = Vector2.new(0.5, 0.5)
+		boolType.BackgroundColor3 = Color3.fromRGB(37, 37, 40)
+		boolType.Position = UDim2.new(0.959999979, 0, 0.5, 0)
+		boolType.Size = UDim2.new(0, 20, 0, 20)
+		boolType.Visible = true
+
+		UICorner_2.CornerRadius = UDim.new(0, 5)
+		UICorner_2.Parent = boolType
+
+		textType.Name = "textType"
+		textType.Parent = Template
+		textType.AnchorPoint = Vector2.new(0.5, 0.5)
+		textType.BackgroundColor3 = Color3.fromRGB(37, 37, 40)
+		textType.Position = UDim2.new(0.899999976, 0, 0.5, 0)
+		textType.Size = UDim2.new(0, 50, 0, 20)
+		textType.Visible = false
+		textType.Font = Enum.Font.GothamMedium
+		textType.PlaceholderText = ".."
+		textType.Text = ""
+		textType.TextColor3 = Color3.fromRGB(255, 255, 255)
+		textType.TextScaled = true
+		textType.TextSize = 14.000
+		textType.TextWrapped = true
+
+		UICorner_3.CornerRadius = UDim.new(0, 5)
+		UICorner_3.Parent = textType
+
+		title.Name = "title"
+		title.Parent = Template
+		title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		title.BackgroundTransparency = 1.000
+		title.Size = UDim2.new(0, 200, 1, 0)
+		title.Text = name
+		title.Font = Enum.Font.GothamBlack
+		title.TextColor3 = Color3.fromRGB(255, 255, 255)
+		title.TextScaled = true
+		title.TextSize = 14.000
+		title.TextWrapped = true
+		title.TextXAlignment = Enum.TextXAlignment.Left
+
+		UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(0.47, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(146, 164, 182))}
+		UIGradient.Rotation = 90
+		UIGradient.Parent = title
+	end
+end
+
 if gameSupported == true then
 	title.Text = "VON "..ver.." | "..MarketPlaceService:GetProductInfo(game.PlaceId).Name
+	featuresLocalization = supported_games[gameId]
+	for i, v in pairs(featuresLocalization) do
+		createFeature(v.type, v.name)
+	end
 else
 	title.Text = "VON "..ver.." | "..MarketPlaceService:GetProductInfo(game.PlaceId).Name.." (Err. 101)"
 end
