25a45a5541c9e16fd12600d829e5c4eb2037773b
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-02-25 13:17:32 +0100

    Update database.lua

--- database.lua
+++ database.lua
@@ -7,6 +7,12 @@ return {
 			["supported_games"] = {
 				[625214705] = {
 					["Args"] = {type = "bool", name = "Args", gameid = 625214705}
+				},
+				[11186098801] = {
+					["Args"] = {type = "bool", name = "Args", gameid = 11186098801},
+					["WalkSpeed"] = {type = "text", name = "WalkSpeed"},
+					["JumpPower"] = {type = "text", name = "JumpPower"},
+					["HipHeight"] = {type = "text", name = "HipHeight"},
 				}
 			}
 
